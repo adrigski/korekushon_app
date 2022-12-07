7cea4fef86747a16e414bd2a0e2144fed3a48ba3
Adrian <32913236+adrigski@users.noreply.github.com>
2022-12-06 19:24:04 -0600

    Implemented ProductView with WebView

---
+++ app/src/main/java/com/example/korekushon_app/ui/settings/SettingsFragment.java
@@ -0,0 +1,23 @@
+package com.example.korekushon_app.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
